6b1ecd2e28985eaa71c7ac7f873333e513a8e65f
alihitawala <Kingnadal1>
2016-02-02 21:41:48 -0600

    Naive Bayes Implementation

--- Bayes.py
+++ Bayes.py
@@ -8,18 +8,46 @@ import math
 def main():
     filename_train = sys.argv[1]
     filename_test = sys.argv[2]
-    m = sys.argv[3]
-    file_train = open(filename_train, 'r')
+    dataset = get_dataset_structure(filename_train)
+    calculate_p_x_y(dataset)
+    test_data_naive_bayes(dataset, filename_test)
+
+
+def test_data_naive_bayes(dataset_train, filename):
+    dataset_test = get_dataset_structure(filename)
+    calculate_p_y_x(dataset_train, dataset_test)
+    print_naive(dataset_test)
+
+
+def print_naive(dataset):
+    for attribute in dataset.attribute_list:
+        if attribute.name == 'class':
+            continue
+        sys.stdout.write(attribute.name + ' class\n')
+    sys.stdout.write('\n')
+    for row in dataset.rows:
+        key_yes = dataset.attributes['class'].values[0].value
+        key_no = dataset.attributes['class'].values[1].value
+        class_key_yes = 'class_'+key_yes
+        class_key_no = 'class_'+key_no
+        if class_key_yes in row:
+            sys.stdout.write(key_yes + ' ' + row['class'] + ' ' + str(row[class_key_yes]) + '\n')
+        else:
+            sys.stdout.write(key_no + ' ' + row['class'] + ' ' + str(row[class_key_no]) + '\n')
+
+
+def get_dataset_structure(filename):
+    file_train = open(filename, 'r')
     attribute_detail_map = dict()
     attributes_list = []
-    isData = False
+    is_data = False
     rows = []
     for line in file_train:
-        wordList = line.split()
-        first_word = wordList[0]
+        word_list = line.split()
+        first_word = word_list[0]
         if first_word == '%':
             continue
-        if isData:
+        if is_data:
             values = [value.strip() for value in line.split(",")]
             attribute_value_map = dict()
             i = 0
@@ -29,29 +57,70 @@ def main():
                 i += 1
             rows.append(attribute_value_map)
         elif first_word == '@attribute':
-            attribute_name = wordList[1].replace("'", "")
+            attribute_name = word_list[1].replace("'", "")
             values_embedded = line[line.find("{")+1:line.find("}")]
             values = [Value(value.strip()) for value in values_embedded.split(",")]
             attrib = Attribute(attribute_name, values)
             attribute_detail_map[attribute_name] = attrib
             attributes_list.append(attrib)
         elif first_word == '@data':
-            isData = True
-    dataset = Dataset(attribute_detail_map, rows)
-    calculate_p_x_y(dataset, attribute_detail_map['class'])
+            is_data = True
+    dataset = Dataset(attributes_list, attribute_detail_map, rows)
+    return dataset
 
 
-def calculate_p_x_y(dataset, attribute_class):
+def calculate_p_y_x(dataset_train, dataset_test):
+    attribute_class = dataset_train.attributes['class']
     param_yes = attribute_class.values[0].value
     param_no = attribute_class.values[1].value
-    for key in dataset.attributes:
-        attribute_info = dataset.attributes[key]
-        for value in attribute_info.values:
-            count_yes = get_count(dataset, attribute_info.name, value, param_yes) * 1.0
-            count_no = get_count(dataset, attribute_info.name, value, param_no) * 1.0
-            value.set_p_x_y_yes(count_yes/(count_no+count_yes))
-            value.set_p_x_y_no(count_no/(count_no+count_yes))
+    p_y_yes = attribute_class.values[0].get_p_x()
+    p_y_no = attribute_class.values[1].get_p_x()
+    for row in dataset_test.rows:
+        p_x_y_yes = p_y_yes
+        p_x_y_no = p_y_no
+        for key in row:
+            if key == 'class':
+                continue
+            attribute_info = dataset_train.attributes[key]
+            values_train = attribute_info.values
+            value_in_test_row = row[key]
+            for value in values_train:
+                if value.value == value_in_test_row:
+                    p_x_y_yes *= value.get_p_x_y_yes()
+                    p_x_y_no *= value.get_p_x_y_no()
+                    break
+        if p_x_y_yes > p_x_y_no:
+            key_new = 'class_' + param_yes
+            row[key_new] = p_x_y_yes/(p_x_y_yes + p_x_y_no)
+        else:
+            key_new = 'class_' + param_no
+            row[key_new] = p_x_y_no/(p_x_y_yes + p_x_y_no)
 
+
+def calculate_p_x_y(dataset):
+    attribute_class = dataset.attributes['class']
+    param_yes = attribute_class.values[0].value
+    param_no = attribute_class.values[1].value
+    count_param_yes = get_count(dataset, 'class', param_yes, param_yes) * 1.0
+    count_param_no = get_count(dataset, 'class', param_no, param_no) * 1.0
+    for key in dataset.attributes:
+        if key == 'class':
+            param_y = attribute_class.values[0]
+            param_y.set_p_x((count_param_yes+1)/(count_param_yes+count_param_no+2))
+            param_n = attribute_class.values[1]
+            param_n.set_p_x((count_param_no+1)/(count_param_yes+count_param_no+2))
+            continue
+        attribute_info = dataset.attributes[key]
+        num_values = len(attribute_info.values)
+        for value in attribute_info.values:
+            count_attribute_in_yes = get_count(dataset, attribute_info.name, value.value, param_yes) * 1.0
+            count_attribute_in_no = get_count(dataset, attribute_info.name, value.value, param_no) * 1.0
+            la_place_num = 1
+            la_place_den = num_values
+            p_x_y_yes = (count_attribute_in_yes + la_place_num) / (count_param_yes + la_place_den)
+            p_x_y_no = (count_attribute_in_no + la_place_num) / (count_param_no + la_place_den)
+            value.set_p_x_y_yes(p_x_y_yes)
+            value.set_p_x_y_no(p_x_y_no)
 
 
 def get_count(dataset, name, value, param):
@@ -69,7 +138,8 @@ class Tuple:
 
 
 class Dataset:
-    def __init__(self, attributes, rows):
+    def __init__(self, attribute_list, attributes, rows):
+        self.attribute_list = attribute_list
         self.attributes = attributes
         self.rows = rows
 
@@ -87,6 +157,13 @@ class Value:
         self.value = value
         self.p_x_y_yes = 0
         self.p_x_y_no = 0
+        self.p_x = 0
+
+    def set_p_x(self, p_x):
+        self.p_x = p_x
+
+    def get_p_x(self):
+        return self.p_x
 
     def set_p_x_y_yes(self, p_x_y):
         self.p_x_y_yes = p_x_y
@@ -99,3 +176,5 @@ class Value:
 
     def get_p_x_y_no(self):
         return self.p_x_y_no
+
+main()
